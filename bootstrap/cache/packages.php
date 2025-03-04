064a3237f1ade813e7266edc77ffac9ed4bff60b
dimas-k <dimasarya81821@gmail.com>
2025-03-04 21:26:28 +0700

    optimasi cache

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,112 @@
+<?php return array (
+  'andes2912/indobank' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Andes2912\\IndoBank\\IndoBankServiceProvider',
+    ),
+    'aliases' => 
+    array (
+    ),
+  ),
+  'asantibanez/livewire-charts' => 
+  array (
+    'aliases' => 
+    array (
+      'LivewireCharts' => 'Asantibanez\\LivewireCharts\\LivewireChartsFacade',
+    ),
+    'providers' => 
+    array (
+      0 => 'Asantibanez\\LivewireCharts\\LivewireChartsServiceProvider',
+    ),
+  ),
+  'biscolab/laravel-recaptcha' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Biscolab\\ReCaptcha\\ReCaptchaServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'ReCaptcha' => 'Biscolab\\ReCaptcha\\Facades\\ReCaptcha',
+    ),
+  ),
+  'enlightn/enlightn' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Enlightn\\Enlightn\\EnlightnServiceProvider',
+    ),
+  ),
+  'laravel/breeze' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Breeze\\BreezeServiceProvider',
+    ),
+  ),
+  'laravel/sail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sail\\SailServiceProvider',
+    ),
+  ),
+  'laravel/sanctum' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sanctum\\SanctumServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'livewire/livewire' => 
+  array (
+    'aliases' => 
+    array (
+      'Livewire' => 'Livewire\\Livewire',
+    ),
+    'providers' => 
+    array (
+      0 => 'Livewire\\LivewireServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'nunomaduro/termwind' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    ),
+  ),
+  'spatie/laravel-ignition' => 
+  array (
+    'aliases' => 
+    array (
+      'Flare' => 'Spatie\\LaravelIgnition\\Facades\\Flare',
+    ),
+    'providers' => 
+    array (
+      0 => 'Spatie\\LaravelIgnition\\IgnitionServiceProvider',
+    ),
+  ),
+);
